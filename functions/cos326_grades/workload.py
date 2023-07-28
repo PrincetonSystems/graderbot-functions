f8972459036f63d5a38da174292293576390800e
kevin <kw22@princeton.edu>
2023-07-27 20:33:07 -0400

    make cos326_grades use updated key

--- functions/cos326_grades/workload.py
+++ functions/cos326_grades/workload.py
@@ -1,35 +1,16 @@
 import csv
-from datetime import datetime, timedelta, timezone
 import io
 import json
+from datetime import datetime, timedelta, timezone
 
-def allocations(length, sum):
-    """Yield all tuples of `length` nonnegative integers that add up to `sum`.
+def find_latest(submissions):
+    """Return the latest in `submissions` while taking into account "fixes" made
+    by graders.
     """
-    if length == 1:
-        yield (sum,)
-    else:
-        for value in range(sum + 1):
-            for permutation in allocations(length - 1, sum - value):
-                yield (value,) + permutation
-
-def find_max(submissions, deadline):
-    """Return the point maximum of `submissions` under a deadline of `deadline`
-    while taking into account "fixes" made by graders.
-    """
-    i, mpoints = 0, 0
-
-    while i < len(submissions) and submissions[i][0] < deadline and not submissions[i][2]:
-        mpoints = max(mpoints, submissions[i][1])
-        i += 1
-
-    if i < len(submissions) and submissions[i][2]:
-        while i < len(submissions):
-            if submissions[i][2]:
-                mpoints = max(mpoints, submissions[i][1])
-            i += 1
-
-    return mpoints
+    i = len(submissions) - 1
+    while i >= 0 and submissions[i][2]:
+        i -= 1
+    return submissions[i] if i >= 0 else None
 
 def handle(req, syscall):
     login = req["login"]
@@ -40,24 +21,25 @@ def handle(req, syscall):
         return { "error": "Only course staff can view grades" }
     assignments = json.loads(syscall.read_key(bytes(f"{req['course']}/assignments", "utf-8")))
     maxes = json.loads(syscall.read_key(bytes(f"{req['course']}/maxes", "utf-8")))
+    deadlines = { asgn: datetime.strptime(assignment["deadline"], "%Y-%m-%dT%H:%M:%S").replace(tzinfo=timezone.utc) for asgn, assignment in assignments.items() }
 
-    student_grades = []
-    student_late_days = []
+    all_student_grades = []
+    all_student_late_days = []
 
     for email in enrollments:
-        # skip staff
-        if enrollments[email]["type"] == "Staff":
+        if enrollments[email]["type"] != "StudentEnrollment":
             continue
 
         name = enrollments[email]["name"].split()
         github = syscall.read_key(bytes(f"users/github/for/user/{email}", "utf-8")).decode("utf-8").strip()
-        student = { "lastname": name[-1], "firstname": name[0], "netid": email[:-14], "github": github }
+        student_grades = { "lastname": name[-1], "firstname": name[0], "netid": email[:-14], "github": github }
+        student_late_days = { "netid": email[:-14] }
 
         autograded = {}
-        late_days = { "netid": email[:-14] }
         for asgn in assignments:
             autograded[asgn] = []
-            late_days[asgn] = 0
+            student_grades[f"{asgn}-autograder"] = None
+            student_late_days[asgn] = 0
             repo = syscall.read_key(bytes(f"{req['course']}/assignments/{asgn}/{email}", "utf-8")).decode("utf-8")
             if repo == "":
                 continue
@@ -65,6 +47,7 @@ def handle(req, syscall):
             all_keys = list(syscall.read_dir(f"github/{repo}"))
             commits = [key for key in all_keys if key.endswith("/") and key != "refs/"]
 
+            # retrieve all autograded grades
             for commit in commits:
                 grade = syscall.read_key(bytes(f"github/{repo}/{commit}grade.json", "utf-8"))
                 if grade == b"":
@@ -73,36 +56,22 @@ def handle(req, syscall):
                 autograded[asgn].append((grade["push_date"], grade.get("given", 0), grade.get("fixed", False)))
             autograded[asgn].sort(key=lambda t: t[0])
 
-            deadline = datetime.strptime(assignments[asgn]["soft_deadline"], "%Y-%m-%dT%H:%M:%S").replace(tzinfo=timezone.utc).timestamp()
-            if len(autograded[asgn]) > 0 and autograded[asgn][-1][0] >= deadline:
-                late_days[asgn] = timedelta(seconds=autograded[asgn][-1][0]-deadline) // timedelta(days=1) + 1
+            extended = (deadlines[asgn] + timedelta(days=1)).timestamp()
+            latest = find_latest(autograded[asgn])
+            student_grades[f"{asgn}-autograder"] = latest[1]
+            if latest and latest[0] >= extended:
+                student_late_days[asgn] = timedelta(seconds=latest[0]-extended) // timedelta(days=1) + 1
 
             # other categories
-            if "extra_grades" in all_keys:
-                extra_grades = json.loads(syscall.read_key(bytes(f"github/{repo}/extra_grades", "utf-8")))
-                fields = [key for key in extra_grades if len(key.split()) == 1]
+            if "extra" in all_keys:
+                extra = json.loads(syscall.read_key(bytes(f"github/{repo}/extra", "utf-8")))
+                fields = [key for key in extra if len(key.split()) == 1]
                 for field in fields:
-                    student[f"{asgn}-{field}"] = extra_grades[field]["earned"]
-
-        max_grade, max_grade_alloc = 0, {}
-        for late_days_alloc in allocations(len(assignments), 4):
-            grade, grade_alloc = 0, {}
-            for i, asgn in enumerate(assignments):
-                extended = (datetime.strptime(assignments[asgn]["soft_deadline"], "%Y-%m-%dT%H:%M:%S").replace(tzinfo=timezone.utc) + timedelta(days=late_days_alloc[i])).timestamp()
-                if "hard_deadline" in assignments[asgn]:
-                    points = find_max(autograded[asgn], min(extended, datetime.strptime(assignments[asgn]["hard_deadline"], "%Y-%m-%dT%H:%M:%S").replace(tzinfo=timezone.utc).timestamp()))
-                else:
-                    points = find_max(autograded[asgn], extended)
-                total_earned = sum([student.get(f"{asgn}-{category}", 0) for category in maxes[asgn] if category != "autograder"]) + points
-                grade += assignments[asgn]["weight"] * total_earned / sum(maxes[asgn].values())
-                grade_alloc[asgn] = points
-            if grade > max_grade:
-                max_grade, max_grade_alloc = grade, grade_alloc
-
-        for asgn in assignments:
-            student[f"{asgn}-autograder"] = max_grade_alloc.get(asgn)
-        student_grades.append(student)
-        student_late_days.append(late_days)
+                    if field not in maxes[asgn]:
+                        return {}
+                    student_grades[f"{asgn}-{field}"] = extra[field]["earned"]
+        all_student_grades.append(student_grades)
+        all_student_late_days.append(student_late_days)
 
     # grades csv
     fieldnames = ["lastname", "firstname", "netid", "github"]
@@ -112,17 +81,17 @@ def handle(req, syscall):
             fieldnames.append(f"{asgn}-{cname}")
             max_student[f"{asgn}-{cname}"] = cmax
 
-    s = io.StringIO()
-    writer = csv.DictWriter(s, fieldnames=fieldnames, extrasaction='ignore')
+    grades_csv = io.StringIO()
+    writer = csv.DictWriter(grades_csv, fieldnames=fieldnames)
     writer.writeheader()
     writer.writerow(max_student)
-    writer.writerows(student_grades)
+    writer.writerows(all_student_grades)
 
     # late days csv
     fieldnames = ["netid"] + list(assignments.keys())
-    s2 = io.StringIO()
-    writer = csv.DictWriter(s2, fieldnames=fieldnames)
+    late_days_csv = io.StringIO()
+    writer = csv.DictWriter(late_days_csv, fieldnames=fieldnames)
     writer.writeheader()
-    writer.writerows(student_late_days)
+    writer.writerows(all_student_late_days)
 
-    return { "grades": s.getvalue(), "late days": s2.getvalue() }
+    return { "grades": grades_csv.getvalue(), "late days": late_days_csv.getvalue() }
